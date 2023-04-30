6a0b9d97cf8d507fa3aaf936495ecac8c947dc20
Bongumusa Nala <bongimusa.nala@gmail.com>
2023-04-30 18:10:56 +0200

    init

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  text = "Default starting text";
+onChangeText(){
+this.text = 'Changed!';
 
-  constructor() {}
+}
+
 
 }
